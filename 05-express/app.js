75c28878e14eb96f68ba8b00b773496c202cb452
Yung Cher Ho <ycho527@hotmail.com>
2020-08-28 14:48:12 +0800

    Initialized views

--- 05-express/app.js
+++ 05-express/app.js
@@ -1,9 +1,28 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const server = http.createServer(app);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Adding middleware before app runs
+// third argument "next" is needed 
+// app.use((req, res, next) => {
+//     console.log('In the middleware');
+//     next(); // Allows the request to continue to next middleware
+// });
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// const server = http.createServer(app);
+// server.listen(3000);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found</h1>');
+})
+
+app.listen(3000); // shortcut to creating a server
