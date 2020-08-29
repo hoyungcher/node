6abe72e838a935caa838a5615dc86906004d268d
Yung Cher Ho <ycho527@hotmail.com>
2020-08-29 23:35:28 +0800

    Added files for different templating engines

--- 06-dynamic-content-templating engines/app.js
+++ 06-dynamic-content-templating engines/app.js
@@ -2,8 +2,21 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+// To use Handlebars, uncomment the line below:
+// const expressHbs = require('express-handlebars');
 
 const app = express();
+
+
+// To use Handlebars, uncomment the line below:
+// app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout'}));
+// app.set('view engine', 'handlebars');
+
+// To use Pug, uncomment the line below:
+// app.set('view engine', 'pug');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -15,7 +28,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
